8cb2df443ebd506cafb74574bf69274ac5e44e88
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-09-21 21:16:24 +0200

    Storing Data with JSON

---
+++ playground/1-json.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+// const book = {
+//   title: 'Javascript',
+//   author: 'Mozilla',
+//   price: 19.99,
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-book.json', bookJSON, 'utf8');
+
+// const dataBuffer = fs.readFileSync('1-book.json', 'utf8');
+// const data = JSON.parse(dataBuffer);
+
+// console.log(data.title);
+
+// const newPerson = fs.readFileSync('1-book.json', 'utf8');
+// const newPersonJSON = JSON.parse(newPerson);
+// newPersonJSON.name = 'Ted';
+// newPersonJSON.age = 34;
+// newPersonJSON.author = 'Mozilla';
+
+// const newData = JSON.stringify(newPersonJSON);
+
+// fs.writeFileSync('1-book.json', newData, 'utf8');
