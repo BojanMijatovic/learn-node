eef9b393c300d891a89d8b1ef67f36530c44a87b
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-09-28 22:25:26 +0200

    add hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,43 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World from express!'));
-app.get('/help', (req, res) => res.send('Help page'));
-app.get('/about', (req, res) => res.send('About page'));
-app.get('/weather', (req, res) => res.send('Weather page'));
+app.set('view engine', 'hbs');
+
+const publicHelpPage = path.join(__dirname, '../public/help.html');
+const publicAboutPage = path.join(__dirname, '../public/about.html');
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather from HBS',
+    name: 'HBS',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'HBS',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'HBS',
+  });
+});
+
+// app.get('/help', (req, res) => {
+//   res.sendFile(publicHelpPage);
+// });
+
+// app.get('/about', (req, res) => {
+//   res.sendFile(publicAboutPage);
+// });
+
+app.get('/weather', (req, res) => res.send({ temp: 30, location: 'London', forecast: 'sunny' }));
 
 app.listen(3000, () => console.log(`Example app listening on port!`));
