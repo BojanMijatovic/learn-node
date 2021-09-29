c92dd6eff0321aa46b43e11aa680e5bc143db317
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-09-29 12:26:10 +0200

    Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,26 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+// const publicHelpPage = path.join(__dirname, '../public/help.html');
+// const publicAboutPage = path.join(__dirname, '../public/about.html');
+
+// set up handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
-const publicHelpPage = path.join(__dirname, '../public/help.html');
-const publicAboutPage = path.join(__dirname, '../public/about.html');
-
+//  set up static directory to serve
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Weather from HBS',
     name: 'HBS',
+    author: 'Alex Read',
   });
 });
 
@@ -20,6 +28,7 @@ app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help',
     name: 'HBS',
+    author: 'Alex Read',
   });
 });
 
@@ -27,6 +36,7 @@ app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About',
     name: 'HBS',
+    author: 'Alex Read',
   });
 });
 
@@ -40,4 +50,5 @@ app.get('/about', (req, res) => {
 
 app.get('/weather', (req, res) => res.send({ temp: 30, location: 'London', forecast: 'sunny' }));
 
+// set up port
 app.listen(3000, () => console.log(`Example app listening on port!`));
