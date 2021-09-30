fe157609d25a5aa957b4c7ff7f4edacd4bd4865b
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-09-30 23:10:45 +0200

    Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,23 @@
-console.log('Client side is on');
+const weatherForm = document.querySelector('form');
+const messageOne = document.querySelector('.message-one');
+const messageTwo = document.querySelector('.message-two');
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = document.querySelector('#location').value;
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    messageOne.textContent = 'Loading...';
+
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = '';
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+
+  document.querySelector('#location').value = '';
+});
